9d2b2a8eec9e853fca6d55273fafa3bbaeaf4b07
Sourabh Bollapragada <chemistry.sourabh@yahoo.co.in>
2016-11-08 21:52:46 -0500

    Initial Client and Server Code

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+
+
+class Client:
+    def __init__(self, ip, port, sip, sport):
+        self.ip = ip
+        self.port = port
+        self.sip = sip
+        self.sport = sport
+
+        # create an INET, STREAMing socket
+
+        # now connect to the web server on port 80
+        # - the normal http port
+
+    def get(self, key):
+        self.socket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.sip, self.sport))
+        self.socket.send("get " + key)
+        msg = self.socket.recv(1000)
+        print msg
+
+    def set(self, key, value):
+        self.socket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.sip, self.sport))
+        self.socket.send("set " + key + " " + value)
+        msg = self.socket.recv(1000)
+        print msg
+
+
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 6000, "127.0.0.1", 5002)
+    client.set("hello", "1000")
+    client.get("hello")
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+
+
+class Server:
+    def __init__(self, ip, port):
+        # create an INET, STREAMing socket
+        self.socket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
+        self.ip = ip
+        self.port = port
+        self.store = {}
+
+    def start(self):
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen(5)
+        while 1:
+            (clientsocket, address) = self.socket.accept()
+            # ct = client_thread(clientsocket)
+            # ct.run()
+            msg = clientsocket.recv(1000)
+            parts = msg.split()
+            print msg
+            if parts[0] == "set":
+                self.set(parts[1], parts[2])
+                clientsocket.send("Done")
+            else:
+                value = self.get(parts[1])
+                clientsocket.send(value)
+
+    def stop(self):
+        pass
+
+    def get(self, key):
+        return self.store[key]
+
+    def set(self, key, value):
+        self.store[key] = value
+
+
+if __name__ == "__main__":
+    server = Server("127.0.0.1", 5002)
+    server.start()
